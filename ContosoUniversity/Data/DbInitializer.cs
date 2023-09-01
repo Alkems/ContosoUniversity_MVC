d06726e84c705eac7f3b1f6150b5fe92c79b9769
Alkems <mynamezalter@gmail.com>
2023-09-01 14:45:30 +0300

    Add data, students.

---
+++ ContosoUniversity/Data/DbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if(context.Students.Any())
+            {
+                return;
+            }
+
+            var students = new Student[]
+            {
+                new Student(){ FirstMidName="Kristjan Georg",LastName="Kessel",EnrollmentDate=DateTime.Now},
+                new Student(){ FirstMidName="Kenneth-Marcus",LastName="Aljas",EnrollmentDate=DateTime.Now},
+                new Student(){ FirstMidName="Karl-Umberto",LastName="Kats",EnrollmentDate=DateTime.Now},
+                new Student(){ FirstMidName="Lebron",LastName="James",EnrollmentDate=DateTime.Now},
+                new Student(){ FirstMidName="Tyrion",LastName="Lannister",EnrollmentDate=DateTime.Now},
+                new Student(){ FirstMidName="Jeffrey",LastName="Epstein",EnrollmentDate=DateTime.Now}
+            };
+
+            context.Students.AddRange(students);
+
+            foreach(Student s in students)
+            {
+                context.Students.Add(s);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -1,8 +1,12 @@
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDbContext<SchoolContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
